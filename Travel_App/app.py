3c5617646f5a55a5c30239bfa67fa16803691288
Marina Ercoli <68237165+Maercoli@users.noreply.github.com>
2021-03-20 12:22:12 -0400

    create engine app.py

--- Travel_App/app.py
+++ Travel_App/app.py
@@ -14,13 +14,44 @@ from flask import (
     request,
     redirect)
 
-
+#################################################
+# Flask Setup
+#################################################
 
 app = Flask(__name__)
 
+#################################################
+# Database Setup
+#################################################
+
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+
+engine=create_engine('postgres://otkchevrhnqetc:72eab30a1c38706e21ea18fbe562411992be9da86472c052eab1300cd9fd5261@ec2-52-71-161-140.compute-1.amazonaws.com:5432/d1tptl61lsnvca') 
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+#set all tables to variables
+Weather_Data=Base.classes.Weather_Data
+
+session = Session(engine)
+
+db = SQLAlchemy(app)
+
+# create route that renders index.html template
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run()
